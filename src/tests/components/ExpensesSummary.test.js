f6a6e32dcc857617545c5a0fbec66b1e52d2b402
Jeremy <netsentry@hush.com>
2020-04-23 08:38:40 -0700

    Setup test database env

--- src/tests/components/ExpensesSummary.test.js
+++ src/tests/components/ExpensesSummary.test.js
@@ -1,17 +1,13 @@
-import React from "react";
-import { shallow } from "enzyme";
-import { ExpensesSummary } from "../../components/ExpensesSummary";
+import React from 'react';
+import { shallow } from 'enzyme';
+import { ExpensesSummary } from '../../components/ExpensesSummary';
 
-test("should render ExpensesSummary correctly with 1 expense", () => {
-  const wrapper = shallow(
-    <ExpensesSummary expenseCount={1} expensesTotal={235} />
-  );
+test('should correctly render ExpensesSummary with 1 expense', () => {
+  const wrapper = shallow(<ExpensesSummary expenseCount={1} expensesTotal={235} />);
   expect(wrapper).toMatchSnapshot();
 });
 
-test("should render ExpensesSummary correctly with multiple expenses", () => {
-  const wrapper = shallow(
-    <ExpensesSummary expenseCount={23} expensesTotal={235324542} />
-  );
+test('should correctly render ExpensesSummary with multiple expenses', () => {
+  const wrapper = shallow(<ExpensesSummary expenseCount={23} expensesTotal={23512340987} />);
   expect(wrapper).toMatchSnapshot();
 });
